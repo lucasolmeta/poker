e21146f7241ec8acd667e5270844532fd75e970d
leolmeta <lolmetab@gmail.com>
2026-02-10 14:04:03 -0600

    working on betting cycle functionality

--- Game.py
+++ Game.py
@@ -9,12 +9,61 @@ class Game:
 
         for i in range(n):
             self.players[i] = Player( self.deck.deal(2) )
-        
+
+        self.pot = 0
+    
+    def betting_round(self):
+        last_aggressor = -1
+        action_on = 0
+        call_amount = 0
+
+        while True:
+            if action_on == last_aggressor:
+                break
+
+            if self.players[ action_on ].folded():
+                action_on = (action_on + 1) % len(self.players)
+                continue
+
+            action = self.players[ action_on ].get_action( call_amount )
+
+            if action[0] == 'fold':
+                continue
+            elif action[0] == 'check':
+                if last_aggressor == -1:
+                    last_aggressor = action_on
+            elif action[0] == 'call':
+                self.pot += action[1]
+
+                if last_aggressor == -1:
+                    last_aggressor = action_on
+            elif action[0] == 'raise':
+                self.pot += action[1]
+                call_amount = action[1]
+
+                last_aggressor = action_on
+
+            action_on = (action_on + 1) % len(self.players)
+
+    def deal_flop(self):
         self.board = Board( self.deck.deal(3) )
+
+    def deal_turn(self):
+        self.board.set_turn( self.deck.deal(1) )
+
+    def deal_river(self):
+        self.board.set_river( self.deck.deal(1) )
+    
+    def burn(self):
+        self.deck.burn()
     
     def print_players(self):
         for i, player in enumerate(self.players):
             print( f'Player {i + 1}: {player.to_str()}' )
+
+    def payout(self):
+        #to be completed
+        return
 
     def print_board(self):
         print( f'Board: {self.board.to_str()}' )
--- Player.py
+++ Player.py
@@ -9,6 +9,29 @@ class Player:
         
         self.cards = cards
         self.balance = 1000
+        self.is_folded = False
+
+    def get_action(self, call_amount):
+        if call_amount == 0:
+            options = "(check, raise, fold)"
+        else:
+            options = f"(call {call_amount}, raise, fold)"
+
+            while True:
+                choice = input(f'What would you like to do? {options}: ').lower().strip()
+                if choice == 'fold':
+                    return ('fold', 0)
+                if choice == 'check' and call_amount == 0:
+                    return ('check', 0)
+                if choice == 'call' and call_amount > 0:
+                    self.balance -= call_amount
+                    return ('call', call_amount)
+                if choice == 'raise':
+                    while True:
+                        raise_amount = input("Enter TOTAL amount to raise to: ")
+                        if raise_amount.isdigit():
+                            self.balance -= (call_amount + int(raise_amount))
+                            return ('raise', int(raise_amount))
     
     def card(self, n: int):
         return self.cards[n]
@@ -16,8 +39,8 @@ class Player:
     def cards(self):
         return self.cards
     
-    def balance(self):
-        return self.balance
+    def folded(self):
+        return self.is_folded
     
     def to_str(self):
         return ", ".join([card.to_str() for card in self.cards if card is not None])
